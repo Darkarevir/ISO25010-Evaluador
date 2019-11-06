fc163717fdddc317152e3a329ed4da73bf0cc6ae
Darkarevir <pablo-11888@hotmail.com>
2019-11-06 09:07:15 -0600

    boton agregado

--- ISO25010_Evaluador/Form1.cs
+++ ISO25010_Evaluador/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace ISO25010_Evaluador
 {
-    public partial class Form1 : Form
+    public partial class Evaluador : Form
     {
-        public Form1()
+        public Evaluador()
         {
             InitializeComponent();
         }
--- ISO25010_Evaluador/Program.cs
+++ ISO25010_Evaluador/Program.cs
@@ -16,7 +16,7 @@ namespace ISO25010_Evaluador
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Evaluador());
         }
     }
 }
