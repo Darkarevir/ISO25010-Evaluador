98a9ff654569cea0730ffdd5b7c9b486e313f4e6
Darkarevir <pablo-11888@hotmail.com>
2019-11-25 17:18:35 -0600

    nuevo form

---
+++ ISO25010_Evaluador/Recuperacion.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace ISO25010_Evaluador
+{
+    partial class Recuperacion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbxSubCaracteristica = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbxCaracteristica = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "SubCaracteristica";
+            // 
+            // cbxSubCaracteristica
+            // 
+            this.cbxSubCaracteristica.FormattingEnabled = true;
+            this.cbxSubCaracteristica.Location = new System.Drawing.Point(25, 91);
+            this.cbxSubCaracteristica.Name = "cbxSubCaracteristica";
+            this.cbxSubCaracteristica.Size = new System.Drawing.Size(223, 21);
+            this.cbxSubCaracteristica.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Caracteristicas";
+            // 
+            // cbxCaracteristica
+            // 
+            this.cbxCaracteristica.FormattingEnabled = true;
+            this.cbxCaracteristica.Location = new System.Drawing.Point(25, 40);
+            this.cbxCaracteristica.Name = "cbxCaracteristica";
+            this.cbxCaracteristica.Size = new System.Drawing.Size(223, 21);
+            this.cbxCaracteristica.TabIndex = 4;
+            // 
+            // Recuperacion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbxSubCaracteristica);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbxCaracteristica);
+            this.Name = "Recuperacion";
+            this.Text = "Recuperacion";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbxSubCaracteristica;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbxCaracteristica;
+    }
+}
